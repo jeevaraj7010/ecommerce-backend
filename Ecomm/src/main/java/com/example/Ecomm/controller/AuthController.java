88116cdd5cc5946f2d348865eb5510a63ac2a4c1
Jeeva <jeevaraj7010@gmail.com>
2026-04-04 18:59:40 +0530

    fixed cors for production

--- Ecomm/src/main/java/com/example/Ecomm/controller/AuthController.java
+++ Ecomm/src/main/java/com/example/Ecomm/controller/AuthController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://ecommerce-frontend-h3as.vercel.app")
 public class AuthController {
 
     private final UserRepository userRepository;
